fedaace5b46e1fc6931bdee4e9cf256b841746c8
Iaroslav <yaroslav.fedchenko@gmail.com>
2025-09-21 21:51:28 +0200

    7

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "modern-normalize/modern-normalize.css";
 import "./index.css";
-import App from "./components/app/App";
+import App from "./components/App/App";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
