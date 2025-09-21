b5577f952772347a0412493902462fc4617d577e
Iaroslav <yaroslav.fedchenko@gmail.com>
2025-09-21 22:07:33 +0200

    8

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import css from "./App.module.css";
+
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
+
 import type { Votes, VoteType } from "../../types/votes";
+
 
 function App() {
   const [votes, setVotes] = useState<Votes>({
